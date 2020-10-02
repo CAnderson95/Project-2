102a57575f1b05d52fc9a31ded70efe3d6bc6486
georgealym <georgealym@gmail.com>
2020-10-01 20:19:58 -0400

    Update Flask app API with information from csv

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,32 +1,21 @@
+#################################################
+# Imports
+#################################################
+
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
-
-
-# Dictionary of Restaurants
-restaurants = [
-    {"Name": "pizza1", "Rating": 5, "Review_Count":600, "Price":"$",
-    "Street_Address":"254 S 2nd St","Zip_code":"11211","Lat":"40.711620","Long":"-73.957830	",
-    "url":"aaaa","category":"pizza"},
-    {"Name": "pizza2", "Rating": 4.5, "Review_Count":80000, "Price":"$$",
-    "Street_Address":"370 W 51st St","Zip_code":"10019","Lat":"40.763940","Long":"-73.988340	",
-    "url":"bbbbb","category":"pizza"},
-    {"Name": "pizza3", "Rating": 4, "Review_Count":500, "Price":"$",
-    "Street_Address":"227 Lenox Ave","Zip_code":"10027","Lat":"40.805587","Long":"-73.947547	",
-    "url":"ccccccc","category":"pizza"},
-    {"Name": "deli1", "Rating": 5, "Review_Count":10, "Price":"$",
-    "Street_Address":"94 Ave B","Zip_code":"10003","Lat":"40.724653","Long":"-73.981702",
-    "url":"ddddd","category":"pizza"},
-    {"Name": "deli2", "Rating": 4.5, "Review_Count":7777777, "Price":"$",
-    "Street_Address":"7803 15th Ave","Zip_code":"11228","Lat":"40.615720","Long":"-74.004820	",
-    "url":"eeeeeee","category":"pizza"},
- 
-]
+from bson import json_util
+from flask_pymongo import PyMongo
+from flask_cors import CORS, cross_origin
+import requests
+import json
+import pandas as pd
 
 #################################################
 # Flask Setup
 #################################################
-app = Flask(__name__)
 
+app = Flask(__name__)
 CORS(app, resources={
     r"/*": {
         "origins": "*"
@@ -34,7 +23,20 @@ CORS(app, resources={
 })
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['CORS_ORIGINS'] = '*'
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Top_100"
+mongo = PyMongo(app)
 
+
+
+######Create Function##### 
+def dataload():
+     restaurants = mongo.db.Top_100 
+     restaurants.drop()
+     data = pd.read_csv("../csv_repository/Overall_List/Top_100_NY_day.csv", encoding = 'ISO-8859-1')
+     restaurants.insert_one(json.loads(data.to_json(orient = "index")))
+
+dataload()
 
 #################################################
 # Flask Routes
@@ -42,9 +44,10 @@ app.config['CORS_ORIGINS'] = '*'
 
 @app.route("/api/v1.0/restaurants")
 def restaurant():
-    """Return the justice league data as json"""
-
-    return jsonify(restaurants)
+    restaurants = mongo.db.Top_100
+    search = restaurants.find({})
+    restaurant_json = json.loads(json_util.dumps(search))
+    return jsonify(restaurant_json)
 
 
 @app.route("/")
@@ -52,11 +55,9 @@ def welcome():
     return (
         f"Welcome to the NYC Popular Restaurant Test API!<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/restaurant"
+        f"/api/v1.0/restaurants"
     )
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
